5db2bee3d1f53912c200ae1a7e269f1860d0d256
Herve Desrosiers <herve_desrosiers@hotmail.com>
2019-12-15 19:37:47 -0500

    chapter 2 challenge 3 changed next and previous buttons to ImageButton objects

--- app/src/main/java/com/example/geoquiz02/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz02/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.geoquiz02
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,8 +15,8 @@ class MainActivity : AppCompatActivity() {
     /////declare ui element variables
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var prevButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
     /////call Question class to create a list of Question objects
