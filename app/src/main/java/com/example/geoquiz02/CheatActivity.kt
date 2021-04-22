d9bbf51fe5ea8030a7d89edab9a79f959e15fbd8
Herve Desrosiers <hdesrosiers@stingray.com>
2021-04-22 09:49:42 -0400

    restart challenge 6a from scratch

--- app/src/main/java/com/example/geoquiz02/CheatActivity.kt
+++ app/src/main/java/com/example/geoquiz02/CheatActivity.kt
@@ -17,9 +17,6 @@ private const val TAG = "CheatActivity"
 private const val EXTRA_ANSWER_IS_TRUE = "com.example.geoquiz02.answer_is_true"
 const val EXTRA_ANSWER_SHOWN = "com.example.geoquiz02.answer_shown"
 
-/////key-value pairs to be stored in onSaveInstanceState Bundle
-private const val KEY_CHEATER = "cheater"
-
 class CheatActivity : AppCompatActivity() {
 
     private lateinit var answerTextView: TextView
@@ -28,20 +25,11 @@ class CheatActivity : AppCompatActivity() {
 
     private var answerIsTrue = false
 
-    /////declare and associate ViewModel
-    private val quizViewModel: QuizViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
 
         Log.d(TAG, "onCreate(Bundle?) of CheatActivity called")
-
-        /////In onCreate(Bundle?), check for values. If they exist, assign them to their respective value name.
-        /////If a value with the key "value" does not exist in the bundle, or if the bundle object is null, set its default value
-        val currentCheat = savedInstanceState?.getBoolean(KEY_CHEATER,false) ?: false
-        Log.d(TAG,"KEY_CHEATER: $currentCheat")
-        quizViewModel.answerMode = currentCheat
 
         answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
         answerTextView = findViewById(R.id.answer_text_view)
@@ -61,19 +49,10 @@ class CheatActivity : AppCompatActivity() {
         apiLevelTextView.text = apiLevel
     }
 
-    /////override onSaveInstanceState(Bundle) to write the value of currentCheat to the bundle with the constant as its key
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        Log.i(TAG,"onSaveInstanceState of CheatActivity")
-        savedInstanceState.putBoolean(KEY_CHEATER, quizViewModel.isCheater)
-        Log.i(TAG,"Cheater: ${quizViewModel.isCheater}")
-    }
-
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
         val data = Intent().apply {
             putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
         }
-        quizViewModel.isCheater = isAnswerShown
         setResult(Activity.RESULT_OK, data)
 
     }
